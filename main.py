1558a30fbb2e0137b300d8389e287e9d05fd8d37
Shubham Kumar <resoshubham2002@gmail.com>
2024-04-13 12:18:27 +0000

    remove errors

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from transformers import pipeline
+# from transformers import pipeline
 
-pipe = pipeline("text-generation", model="georgesung/llama2_7b_chat_uncensored")
-prompt_text = ("hi ")
-generated_text = pipe(prompt_text, num_return_sequences=1)
+# pipe = pipeline("text-generation", model="georgesung/llama2_7b_chat_uncensored")
 
-print(generated_text[0]['generated_text'])
+# model = AutoModelForCausalLM.from_pretrained("FlagAlpha/Llama2-Chinese-7b-Chat")
+# prompt_text = ("hi ")
+# generated_text = pipe(prompt_text, num_return_sequences=1)
+
+# print(generated_text[0]['generated_text'])
+from transformers import AutoTokenizer, LlamaForCausalLM
+import os
+os.environ["HF_TOKEN"] = ""
+
+model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]

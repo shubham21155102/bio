c642d2c9f7b6eac565927c4aee500afd64337754
Shubham Kumar <resoshubham2002@gmail.com>
2024-04-13 12:45:48 +0000

    add changes

--- main.py
+++ main.py
@@ -10,13 +10,14 @@
 from transformers import AutoTokenizer, LlamaForCausalLM
 import os
 os.environ["HF_TOKEN"] = ""
-
+cache_dir = "/workspace/bio/models/"
+# os.makedirs(cache_dir, exist_ok=True)
 model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 
 prompt = "Hey, are you conscious? Can you talk to me?"
 inputs = tokenizer(prompt, return_tensors="pt")
 
-# Generate
 generate_ids = model.generate(inputs.input_ids, max_length=30)
-tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+decoded_output =tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+print(decoded_output)
